26d916402ac0d62fd2d13800146861bbdebc71a0
anoopuchil <30733929+anoopuchil@users.noreply.github.com>
2017-08-08 15:45:58 -0400

    Updated probe
    Working on increment counter and Progress bar.

--- Probe.ps1
+++ Probe.ps1
@@ -1,11 +1,18 @@
-﻿#Date 8/4/2017
-
-#Run as Admin
+﻿#Run as Admin
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
+#Increment Counter
 
-#Variable for Output File Path to current desktop (Old; for research)
-#$UserPath = "$($env:USERPROFILE)\Desktop\$env:computername.printerExport"
+$incre = 0
+
+function addthis() {
+    #Add this
+
+    $global:incre++
+    
+}
+
+
 
 
 #Variable for current path
@@ -15,24 +22,28 @@ $dir = Split-Path $scriptpath
 #Default variable for output file
 $Fname=$env:computername
 
+#WIP:  {Write-Progress -Activity "Search in Progress" -Status "Running Script $incre" -PercentComplete(($incre/5)*100)}
 
 
+#gpresult
+addthis
 gpresult /r /scope:computer 																				| Out-File $dir\$fname.txt -append
-ipconfig /all 																								| Out-File $dir\$fname.txt -append
+
+#Network Info
+addthis
+Get-WmiObject win32_networkadapterconfiguration | select INDEX,DESCRIPTION,MACADDRESS,IPADDRESS	     		| Out-File $dir\$fname.txt -append
 
 #List Connected Printers
-get-printer																									| Out-File $dir\$fname.txt -append
+addthis
+get-printer | Select NAME,DRIVERNAME,PORTNAME																| Out-File $dir\$fname.txt -append
 
 #List Mapped Shared Drives 
-Get-WmiObject -Class Win32_MappedLogicalDisk | select Name, ProviderName									| Out-File $dir\$fname.txt -append
+addthis
+Get-WmiObject -Class Win32_MappedLogicalDisk | select NAME, PROVIDERNAME									| Out-File $dir\$fname.txt -append
 
 
-#List Users that are Admins on this computer
-$userToFind = $args[0] 
-$administratorsAccount = Get-WmiObject Win32_Group -filter "LocalAccount=True AND SID='S-1-5-32-544'" 
-$administratorQuery = "GroupComponent = `"Win32_Group.Domain='" + $administratorsAccount.Domain + "',NAME='" + $administratorsAccount.Name + "'`"" 
-$user = Get-WmiObject Win32_GroupUser -filter $administratorQuery | select PartComponent |where {$_ -match $userToFind} 
-$user 																										| Out-File $dir\$fname.txt -append
+#List Users on this computer
+addthis
+Get-WmiObject -Class Win32_UserAccount | Select CAPTION,DOMAIN,NAME,FULLNAME								| Out-File $dir\$fname.txt -append
 
-
-pause
+Write-Host "Probe Complete!"
